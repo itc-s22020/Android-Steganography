de2bfb10bc157377cf4d9193aed1dc65fc6efc3c
s22004 <s22004@std.it-college.ac.jp>
2023-12-19 11:55:52 +0900

    navigationとハンバーガーを少し 訂正

--- app/src/main/java/jp/ac/it_college/stds/androidsteganography/SteganoNavigation.kt
+++ app/src/main/java/jp/ac/it_college/stds/androidsteganography/SteganoNavigation.kt
@@ -1,15 +1,12 @@
 package jp.ac.it_college.stds.androidsteganography
 
 import android.graphics.Bitmap
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.CenterAlignedTopAppBar
-import androidx.compose.material3.DrawerValue
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -17,9 +14,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,10 +22,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color.Companion.Yellow
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
-import androidx.navigation.NavBackStackEntry
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
@@ -64,18 +56,8 @@ fun SteganoNavigation(
     }
     var showText by remember { mutableStateOf(false) }
 
-
-
     val context = LocalContext.current
     val scope = rememberCoroutineScope()
-
-    val items = listOf(
-        "Inbox",
-        "Starred",
-        "Sent",
-        "Drafts",
-        "Settings"
-    )
 
     Surface(
         modifier = Modifier.fillMaxSize(),
