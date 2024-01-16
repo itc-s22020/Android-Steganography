24c08ec1f5eb6e769b58a800c2a3ff1d95688b45
itc-s22020 <s22020@std.it-college.ac.jp>
2024-01-16 10:37:28 +0900

    StartSceneにいろいろ配置

--- app/src/main/java/jp/ac/it_college/stds/androidsteganography/scene/StartScene.kt
+++ app/src/main/java/jp/ac/it_college/stds/androidsteganography/scene/StartScene.kt
@@ -1,10 +1,15 @@
 package jp.ac.it_college.stds.androidsteganography.scene
 
 import android.graphics.Bitmap
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedCard
@@ -16,21 +21,19 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.HorizontalAlignmentLine
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import jp.ac.it_college.stds.androidsteganography.components.common.ImagePickerView
 import jp.ac.it_college.stds.androidsteganography.ui.theme.AndroidSteganographyTheme
-import kotlin.math.roundToInt
 
 @Composable
 fun StartScene(modifier: Modifier = Modifier, onEncryptClick: (Bitmap) -> Unit = {}, onDecryptClick: (Bitmap) -> Unit = {})  {
     var bm by remember { mutableStateOf(Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888)) }
     val initialBitmap by remember { mutableStateOf(Bitmap.createBitmap(3500, 2200, Bitmap.Config.ARGB_8888)) }
     Column (
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize()
+            .padding(10.dp),
     ) {
         OutlinedCard(
             modifier = Modifier
@@ -51,15 +54,23 @@ fun StartScene(modifier: Modifier = Modifier, onEncryptClick: (Bitmap) -> Unit =
         }
         
         Text(text = "保存できる推定容量: ${bm.width*bm.height*3/8} byte", modifier.align(Alignment.CenterHorizontally))
-        Button(onClick = {onEncryptClick(bm)},
-            enabled = bm.width + bm.height > 2,
-            modifier = modifier.align(Alignment.CenterHorizontally)) {
-            Text("暗号化")
-        }
-        Button(onClick = { onDecryptClick(bm) },
-            enabled = bm.width + bm.height > 2,
-            modifier = modifier.align(Alignment.CenterHorizontally)) {
-            Text("復号化")
+        Row(
+            horizontalArrangement = Arrangement.Center,
+            modifier = Modifier.fillMaxWidth(1f),
+        ) {
+            Button(
+                onClick = { onEncryptClick(bm) },
+                enabled = bm.width + bm.height > 2,
+            ) {
+                Text("暗号化")
+            }
+            Spacer(modifier = Modifier.width(30.dp))
+            Button(
+                onClick = { onDecryptClick(bm) },
+                enabled = bm.width + bm.height > 2,
+            ) {
+                Text("復号化")
+            }
         }
 
     }
